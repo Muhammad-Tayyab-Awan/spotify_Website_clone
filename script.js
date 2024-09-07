b27e674f1fbd96b3d904c447bd6541e811e99652
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-07 20:37:43 +0000

    Sep 8, 2024, 1:37 AM

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 async function getSongs() {
   let fetchSongs = await fetch("http://localhost:5500/songs/");
   let response = await fetchSongs.text();
-  console.log(response);
+  let el = document.createElement("div");
+  el.innerHTML = response;
+    let songLinks = div.getElementByTagName("a");
+    let songs = [];
+    for (let i = 0; i < songLinks.length; i++) {
+        const element = array[i];
+        
+    }
 }
 getSongs();
