cb22fd6affba6c6a06953087bd94c09fd31dd32b
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-07 20:39:48 +0000

    Sep 8, 2024, 1:39 AM

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ async function getSongs() {
   el.innerHTML = response;
   let songLinks = div.getElementByTagName("a");
   let songs = [];
-    for (let i = 0; i < songLinks.length; i++) {
-    if(songLinks[0].endsWi)
-        songs.push(songLinks[0])
-    
+  for (let i = 0; i < songLinks.length; i++) {
+    if (songLinks[0].href.endsWith(".mp3")) {
+      songs.push(songLinks[0]);
+    }
   }
+  console.log(songs);
 }
 getSongs();
