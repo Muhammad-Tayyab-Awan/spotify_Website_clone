4f8aff273a2da96b4b0078cbe32cbd51dfee9346
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-09 08:22:41 +0000

    Sep 9, 2024, 1:22 PM

--- script.js
+++ script.js
@@ -90,7 +90,6 @@ async function main() {
   let nextSong = document.querySelector(".next");
   nextSong.addEventListener("click", (evt) => {
     if (songs.indexOf(currentSong.src) + 1 < songs.length) {
-      console.log(songs.indexOf(currentSong.src) + 1, songs.length);
       playMusic(
         decodeURI(songs[songs.indexOf(currentSong.src) + 1].split("/songs/")[1])
       );
