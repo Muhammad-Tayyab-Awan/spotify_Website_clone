02a31d6ed5c81bab1d8288c1a3f918c28cb2d806
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-09 11:42:16 +0000

    Sep 9, 2024, 4:42 PM

--- script.js
+++ script.js
@@ -106,14 +106,14 @@ async function getAlbum() {
   let albums = [];
   for (let i = 0; i < albumLinks.length; i++) {
     if (albumLinks[i].href.startsWith("http://127.0.0.1:5500/songs/")) {
-      albums.push(albumLinks[i].href.split("/songs/")[1]);
+      albumsName.push(albumLinks[i].href.split("/songs/")[1]);
     }
   }
   return {albumsName,albumsData};
 }
 async function main() {
   let albums = await getAlbum();
-  currAlbum = albums.albumName[0];
+  currAlbum = albums.albumsName[0];
   let albumContainer = document.querySelector(".cardContainer");
   albums.forEach((e) => {
     let albumCard = document.createElement("div");
