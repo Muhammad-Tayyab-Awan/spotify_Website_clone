d2f7b20c6937a70bf641684c6624a5bb17795f67
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-09 05:42:08 +0000

    Sep 9, 2024, 10:42 AM

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function convertSecondsToTime(seconds) {
   const remainingSeconds = seconds % 60;
   const paddedMinutes = String(minutes).padStart(2, "0");
   const paddedSeconds = String(remainingSeconds).padStart(2, "0");
-  return `${paddedMinutes}:${Math.ceil(paddedSeconds)}`;
+  return `${paddedMinutes}:${Math.floor(paddedSeconds)}`;
 }
 
 const playMusic = (track) => {
