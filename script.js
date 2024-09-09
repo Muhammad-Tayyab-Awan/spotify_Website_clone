b5dca24cf31257550a6d66e7376ca7c90c4d236c
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-09 11:55:43 +0000

    Sep 9, 2024, 4:55 PM

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ const playMusic = (track, pause = false) => {
 };
 async function getAlbumData(path) {
   let response = await fetch(`http://127.0.0.1:5500/songs/${path}/info.json`);
-  let jsonResponse = await response.json;
+  let jsonResponse = await response.json();
   console.log(jsonResponse);
   return jsonResponse;
 }
