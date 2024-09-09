941c5262c8f948d7db727b3b7ef82eb54fe89f04
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-09 05:29:54 +0000

    Sep 9, 2024, 10:29 AM

--- script.js
+++ script.js
@@ -21,7 +21,11 @@ const playMusic = (track) => {
   document.querySelector(".songsInfo").innerText = track.split(".")[0];
   // document.querySelector(".totalDur").innerText = currentSong.duration;
   currentSong.addEventListener("timeupdate", () => {
-    console.log(Math.floor(currentSong.duration) / 60);
+    console.log(
+      eval(
+        (Math.floor(currentSong.duration) / 60).toString().split(".")[1] + "*60"
+      )
+    );
   });
   playPause.classList.remove("fa-circle-play");
   playPause.classList.add("fa-circle-pause");
