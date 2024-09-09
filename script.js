3fb3def0ed5b781345aca6bb7bd1af15f5993db3
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-09 07:20:41 +0000

    Sep 9, 2024, 12:20 PM

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ async function main() {
     let y = evt.clientY - rect.top;
     x = (x / rect.width) * 100;
     y = (y / rect.height) * 100;
-    currentSong.currentTime = y
+    currentSong.currentTime = x
     document.querySelector(".circle").style.left = `${x.toFixed(2)}%`;
   });
 }
